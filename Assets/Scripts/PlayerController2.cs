5df6e2a56377a8cf5c86d1caecc544953e8bc617
nf76398n <nf76398n@pace.edu>
2024-12-09 19:07:19 -0500

    Added More GamePlay elements to Scene 2
    Fixed puzzle, finished overall Scene 2 by adding maze, added Enemy, trigger mechanics, and key

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -5,33 +5,66 @@ using TMPro;
 
 public class PlayerController2 : MonoBehaviour
 {
-     public TMP_Text objectiveText;
+    public TMP_Text objectiveText;
+    public Transform spawnPoint;
+    public GameObject Enemy;
+
+    bool hasTriggered = false;
+    bool hasTriggered2 = false;
+    bool hasTriggered3 = false;
+    bool hasDestroyed = false;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        objectiveText.text = "Objective: Solve the puzzles";
+        objectiveText.text = "Objective: Escape";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameData.balls == 4) {
+            Destroy(GameObject.FindGameObjectWithTag("FakeWall"));
+        }
     }
 
     private void OnTriggerEnter(Collider other) 
     {
-        // if ()) {
-        //     Destroy(other.gameObject);
-        // }
-        if (other.CompareTag("WallTrigger")) 
+        if (other.CompareTag("WallTrigger") && hasTriggered == false) 
         {
             objectiveText.text = "Objective: Solve the puzzle";
+            bool hasTriggered = true;
         }
-        if (GameData.points == 3 && other.CompareTag("NPC")) {
+        if (other.CompareTag("WallTrigger2") && hasTriggered2 == false) {
+            objectiveText.text = "Objective: Escape";
+            bool hasTriggered2 = true;
+        }
+        if (other.CompareTag("WallTrigger3") && hasTriggered3 == false) {
+            objectiveText.text = "Objective: Escape this maze";
+            bool hasTriggered3 = true;
+        }
+        if (other.CompareTag("Key")) {
+            objectiveText.text = "Objective: Escape";
+            Destroy(other.gameObject);
+            hasDestroyed = true;
+        }
+        if (other.CompareTag("NPC") && hasDestroyed == true) {
             objectiveText.text = "";
             UnityEngine.SceneManagement.SceneManager.LoadScene("Level3");
+        }
+        else if (other.CompareTag("NPC") && hasDestroyed == false) {
+            objectiveText.text = "NPC: You're missing the lie to this maze";
+        }
+        if (other.CompareTag("EnemyTrigger") && hasDestroyed == true) {
+            objectiveText.text = "Objective: Avoid the monster and escape";
+            Instantiate(Enemy, spawnPoint.position, spawnPoint.rotation);
+
+        }
+        if (other.CompareTag("AI")) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Lose");
         }
     }
 }
