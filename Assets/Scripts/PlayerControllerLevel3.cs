e77994fd2c961865c114dfd30ed4400cffd22767
Wilson Lu <wl54321n@pace.edu>
2024-12-16 20:50:06 -0500

    added level 3 changes

--- Assets/Scripts/PlayerControllerLevel3.cs
+++ Assets/Scripts/PlayerControllerLevel3.cs
@@ -1,30 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerControllerLevel3 : MonoBehaviour
 {
     public Camera cam; 
-    public UnityEngine.AI.NavMeshAgent monster;
+    UnityEngine.AI.NavMeshAgent monster;
+    public TMP_Text objectiveText;
+    public float timer = 0f;
+    bool first = true;
     // Start is called before the first frame update
     void Start()
     {
-       GameData.points = 0;
-        Cursor.visible = true;
+        GameData.points = 0;
         monster = GameObject.FindGameObjectWithTag("AI").GetComponent<UnityEngine.AI.NavMeshAgent>();
+        objectiveText.text = "the mannequinn use all their power to grant you a skill\ntry it out press x";
+        first = true;
     }
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);   
-        if (Physics.Raycast(ray, out hit)) {
-            Transform objectHit = hit.transform;
-            if (objectHit.name == "Skeleton") {
+        if (timer <= 8 && monster.speed == 0) {
+            first = false;
+            monster.speed = 3.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.X)) {
+            if (timer <= 0) {
+                if (first) {
+                    objectiveText.text = "it freeze the monster, there a big cooldown to use it again";
+                }
                 monster.speed = 0;
-            } else {
-                monster.speed = 3.5f;
+                timer = 10f;
             }
+        }
+        timer -= Time.deltaTime;
+        if (timer > 0 && !first) {
+            objectiveText.text = "cooldown: " + timer.ToString();
+        } else if (timer <= 0 && !first) {
+            objectiveText.text = "you can use x skill again";
         }
     }
     
